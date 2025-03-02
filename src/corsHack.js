7b1c3b37fe100ec28279a880fdb5d1bc587ab526
danieljemoore <dm5q3@mst.edu>
2025-03-02 08:13:31 -0600

    cors

--- src/corsHack.js
+++ src/corsHack.js
@@ -1,6 +1,6 @@
 // src/config.js
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const API_BASE_URL = isDevelopment
+export const API_BASE_URL = !isDevelopment
   ? '/api'
-  : 'https://neuralnetworth-backend-409815903554.us-central1.run.app/api';
+  : 'https://neuralnetworth-backend-409815903554.us-central1.run.app';
